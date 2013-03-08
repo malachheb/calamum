c2ebeee4c45edd8154a99b05f30bc11cc5fd60b1
Mehrez Alachheb <m.alachheb@alphalink.fr>
2013-03-08 17:10:08 +0100

    Add headers attribute for request class

--- lib/calamum/request.rb
+++ lib/calamum/request.rb
@@ -1,26 +1,40 @@
 class Calamum::Request
   include ActiveModel::Validations
-  
+
   attr_reader :errors
-  attr_accessor :uri, :action, :params, :content_type, :description
+  attr_accessor :uri, :action, :params, :description, :headers, :response
   ACTIONS= %{GET POST PUT DELETE}
-  
+
   validates :uri, :action, :presence => true
   # validates :uri, :uniqueness => true
-  validates :action, :inclusion => { :in => ACTIONS, :message => "is not included in [#{ACTIONS}]"} 
-    
+  validates :action, :inclusion => { :in => ACTIONS, :message => "is not included in [#{ACTIONS}]"}
+
   def initialize(attrs)
     @uri= attrs[:uri]
     @action= attrs[:action].upcase
     @params= attrs[:params] || {}
-    @content_type = attrs[:content_type]
+    @headers = attrs[:headers] || {}
+    @response = attrs[:response] || ''
     @description = attrs[:description]
     @errors = ActiveModel::Errors.new(self)
     self.valid?
   end
-  
+
+  def action_label
+    case @action
+    when 'GET'
+      'label-info'
+    when 'POST'
+      'label-success'
+    when 'PUT'
+      'label-warning'
+    when 'DELETE'
+      'label-important'
+    end
+  end
+
   def to_s
     "#{action}: #{uri}"
   end
-  
+
 end
--- spec/definition_parser_spec.rb
+++ spec/definition_parser_spec.rb
@@ -8,10 +8,10 @@ describe Calamum::DefinitionParser do
   end
 
   specify{@api_definition.get_resources_names.should == ['user', 'group']}
-  specify{@api_definition.get_resource_uris('user').size.should == 2}
-  specify{@api_definition.resources['user'].size.should == 2}
+  specify{@api_definition.get_resource_uris('user').size.should == 3}
+  specify{@api_definition.resources['user'].size.should == 3}
   specify{@api_definition.resources['user'][0].errors.should be_empty}
   specify{@api_definition.resources['user'][0].action.should == 'GET'}
   specify{@api_definition.resources['user'][0].uri.should == '/users'}
-  specify{@api_definition.resources['user'][0].content_type.should == 'application/json'}
+  specify{@api_definition.resources['user'][1].headers.should_not be_empty}
 end
